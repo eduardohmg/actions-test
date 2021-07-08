62cc35847daf11add12e6303ec339fd071208ed6
Eduardo <eduardohmgarcia@gmail.com>
2021-07-08 16:14:49 +0200

    Endpoint test

---
+++ src/main/java/com/example/springboot/endpoint/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.springboot.endpoint;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorld {
+
+    @GetMapping("/hello-world")
+    @ResponseBody
+    public String sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        return "Hello, " + name;
+    }
+}
